69648b87cfa18bfec60ea928654a330a96069873
Your Name <your-email@example.com>
2025-07-28 14:48:13 +0500

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+       
+        public int CategoryId { get; set; }
+        [Required]
+        [DisplayName("Category Name")] // this is data anotation it will display category name instead on name
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
