a929895b2842afbe885e68396185b16489e190c1
Your Name <your-email@example.com>
2025-07-31 12:36:10 +0500

    Crud operation with Catergories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BulkyWeb.Controllers
 {
@@ -9,7 +10,7 @@ namespace BulkyWeb.Controllers
         // CategoryController constructor
 
 
-        private readonly ApplicationDbContext _db;
+        private readonly ApplicationDbContext _db;   
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
@@ -27,9 +28,97 @@ namespace BulkyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+
+            //custom validation
+           
+            //check for data validation at server side
+            if (!ModelState.IsValid) { 
+           
+                return View();
+            }
+            if (obj.Name.ToLower() == obj.Description.ToLower())
+            {
+                ModelState.AddModelError("name", "Description and name cannot be same");
+                return View();
+            }
             _db.Categories.Add(obj);
             _db.SaveChanges();
-            return RedirectToAction("Index","Catogry");
+            TempData["success"] = "Category  Created Successfully";
+            return RedirectToAction("Index", "Category");
+
+        }
+
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            //  One way of retrieving category 
+            Category? categoryFromDb = _db.Categories.Find(id);  // only work with primary key 
+            Category? catergoryFromDb1 = _db.Categories.FirstOrDefault(u => u.CategoryId == id); // can find base on ay condition(fiels)
+            Category? catergoryFromDb2 = _db.Categories.Where(u => u.CategoryId == id).FirstOrDefault();// another way of finding any field
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        { 
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category  Updated Successfully";
+                return RedirectToAction("Index", "Category");
+
+                
+            }
+            return View();
+
+
+
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //  One way of retrieving category 
+            Category? categoryFromDb = _db.Categories.Find(id);  // only work with primary key 
+            //Category? catergoryFromDb1 = _db.Categories.FirstOrDefault(u => u.CategoryId == id); // can find base on ay condition(fiels)
+            //Category? catergoryFromDb2 = _db.Categories.Where(u => u.CategoryId == id).FirstOrDefault();// another way of finding any field
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+              
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category  Deleted Successfully";
+            return RedirectToAction("Index");
+
+
+
+
+
         }
     }
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -5,11 +5,14 @@ namespace BulkyWeb.Models
 {
     public class Category
     {
-       
+        [Key]
         public int CategoryId { get; set; }
         [Required]
+        [MaxLength(30)]
         [DisplayName("Category Name")] // this is data anotation it will display category name instead on name
         public string Name { get; set; }
+        [Required]
+        [MaxLength(30,ErrorMessage ="Only 30 word are allowed")]
         public string Description { get; set; }
     }
 }
